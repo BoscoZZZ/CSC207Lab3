25b095a95d4eb165942c1860afb388b9bf2d143e
Juntongdalao <eric.superking.6626@gmail.com>
2024-09-23 12:16:24 -0400

    erictest1

--- src/main/java/org/translation/JSONTranslationExample.java
+++ src/main/java/org/translation/JSONTranslationExample.java
@@ -23,7 +23,8 @@ public class JSONTranslationExample {
             // which we then create a new JSONArray object from.
             // TODO CheckStyle: Line is longer than 120 characters
             //                  (note: you can split a line such that the next line starts with a .method()... call
-            String jsonString = Files.readString(Paths.get(getClass().getClassLoader().getResource("sample.json").toURI()));
+            String jsonString = Files.readString(
+                    Paths.get(getClass().getClassLoader().getResource("sample.json").toURI()));
             this.jsonArray = new JSONArray(jsonString);
         }
         catch (IOException | URISyntaxException ex) {
@@ -52,6 +53,13 @@ public class JSONTranslationExample {
      * @return the translation of country to the given language or "Country not found" if there is no translation.
      */
     public String getCountryNameTranslation(String countryCode, String languageCode) {
+        CountryCodeConverter countryCodeConverter = new CountryCodeConverter();
+        String name = countryCodeConverter.fromCountryCode(countryCode);
+        if (name != null) {
+            InLabByHandTranslator inLabByHandTranslator = new InLabByHandTranslator();
+            return inLabByHandTranslator.translate(languageCode, name);
+        }
+
         return "Country not found";
     }
 
